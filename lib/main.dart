2a0bc3d6d389f816d05c5c5cadf7294126b90f16
sownt <thonsai@outlook.com>
2023-04-29 18:43:59 +0700

    fix crash/remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:cloudskillsboost_profile_validator/util/routes.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
